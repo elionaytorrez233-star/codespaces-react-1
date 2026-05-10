5b6cbbb0faa9639ef676892b3cb4b6471c3eed17
elionaytorrez233-star <elionaytorrez233@gmail.com>
2026-05-10 06:40:19 +0000

    corregir YAML del deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    globals: true,
-    environment: 'jsdom',
-  base: '/codespaces-react-1/' //
-
+  base: '/codespaces-react-1/',
+});
